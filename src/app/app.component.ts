d95a5bdaf86f63d604261d09d59fbb851fbe0bac
Emircan <emircandv787@gmail.com>
2024-11-09 20:53:22 +0300

    The custom modal component and sample have been added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Sample1Component } from './custom-navbar/samples/sample1/sample1.component';
+import { Sample1Component } from './custom-modal/samples/sample1/sample1.component';
+// import { Sample1Component } from './custom-navbar/samples/sample1/sample1.component';
 // import { Sample1Component } from './custom-card/samples/sample1/sample1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,Sample1Component],
+  imports: [
+    RouterOutlet,
+    Sample1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/custom-modal/samples/sample1/sample1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ModalComponent } from "../../components/modal/modal.component";
+
+@Component({
+  selector: 'cm-sample1',
+  standalone: true,
+  imports: [ModalComponent],
+  templateUrl: './sample1.component.html',
+  styleUrl: './sample1.component.scss'
+})
+export class Sample1Component {
+
+}
